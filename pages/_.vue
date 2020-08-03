8b290046c4b5029a9b67dd96ad1f36d0e857b811
Avris <andre@avris.it>
2020-08-03 19:47:53 +0200

    #33 skrócone tablice odmiany

--- pages/_.vue
+++ pages/_.vue
@@ -39,6 +39,64 @@
             </ul>
         </section>
 
+        <section>
+            <h2 class="h4">
+                <Icon v="spell-check"/>
+                Odmiana:
+            </h2>
+
+            <div class="table-responsive">
+                <table class="table table-sm">
+                    <thead>
+                    <tr>
+                        <th>Mianownik</th>
+                        <th>Dopełniacz</th>
+                        <th>Celownik</th>
+                        <th>Biernik</th>
+                        <th>Narzędnik</th>
+                        <th>Miejscownik</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_n') }}</td>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_g') }} / {{ selectedTemplate.getMorpheme('pronoun_g_acc') }}</td>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_d') }}</td>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_a') }}</td>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_i') }}</td>
+                        <td>{{ selectedTemplate.getMorpheme('pronoun_l') }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="table-responsive">
+                <table class="table table-sm">
+                    <thead>
+                    <tr>
+                        <th>1 os.</th>
+                        <th>2 os.</th>
+                        <th>3 os.</th>
+                        <th>Przymiotniki</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>-{{ selectedTemplate.getMorpheme('verb_middle') }}{{ selectedTemplate.plural ? 'śmy' : 'm'}}</td>
+                        <td>-{{ selectedTemplate.getMorpheme('verb_middle') }}{{ selectedTemplate.plural ? 'ście' : 'ś'}}</td>
+                        <td>-{{ selectedTemplate.getMorpheme('verb_end_about') }}</td>
+                        <td>-{{ selectedTemplate.getMorpheme('adjective_n') }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!--
+
+            pronoun_all	adjective_n	adjective_ll	adjective_middle	verb_end_inter	verb_end_about	verb_middle	verb_nasal	verb_go	verb_o
+            -->
+        </section>
+
         <section v-if="selectedTemplate.history">
             <div class="alert alert-info">
                 <Icon v="info-circle"/>
