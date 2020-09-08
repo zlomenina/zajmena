0567da451ad429f56b4ac1dbe54f8c044039a512
Avris <andre@avris.it>
2020-09-09 00:10:31 +0200

    [homepage] przyjaźniejszy interfejs kreatorów

--- pages/index.vue
+++ pages/index.vue
@@ -25,10 +25,6 @@
         <Separator icon="link"/>
 
         <section>
-            <p>
-                Wybierz sposród najpopularniejszych:
-            </p>
-
             <ul class="list-group">
                 <li v-for="[group, groupTemplates] in templateLibrary.split()" class="list-group-item">
                     <p class="h5">
@@ -51,90 +47,85 @@
                         </li>
                     </ul>
                 </li>
-            </ul>
-        </section>
-
-        <section>
-            <p>
-                Wybierz formy wymienne:
-            </p>
-
-            <a v-if="!customiseMultiple" href="#" @click.prevent="customiseMultiple = true" class="btn btn-outline-primary btn-block">
-                <Icon v="sliders-h-square"/>
-                Wybierz formy wymienne
-            </a>
-            <div v-else class="card mb-5">
-                <div class="card-header">
-                    <Icon v="sliders-h-square"/>
-                    Formy wymienne:
-                </div>
-                <div class="card-body">
-                    <div class="card-title">
-                        <ul class="list-inline d-inline mb-0">
-                            <li class="list-inline-item" v-for="(template, pronoun) in templates">
-                                <button :class="['btn', multiple.includes(pronoun) ? 'btn-primary' : 'btn-outline-primary', 'btn-sm', 'my-1']"
-                                        @click="toggleMultiple(pronoun)"
-                                >
-                                    {{template.name()}}
-                                </button>
-                            </li>
-                        </ul>
+                <li class="list-group-item">
+                    <a v-if="!customiseMultiple" href="#" @click.prevent="customiseMultiple = true" class="btn btn-outline-primary btn-block">
+                        <Icon v="sliders-h-square"/>
+                        Wygeneruj link do form wymiennych
+                    </a>
+                    <div v-else class="card">
+                        <div class="card-header">
+                            <Icon v="sliders-h-square"/>
+                            Formy wymienne:
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">
+                                <ul class="list-inline d-inline mb-0">
+                                    <li class="list-inline-item" v-for="(template, pronoun) in templates">
+                                        <button :class="['btn', multiple.includes(pronoun) ? 'btn-primary' : 'btn-outline-primary', 'btn-sm', 'my-1']"
+                                                @click="toggleMultiple(pronoun)"
+                                        >
+                                            {{template.name()}}
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card-footer" v-if="linkMultiple">
+                            <LinkInput :link="linkMultiple"/>
+                        </div>
                     </div>
-                </div>
-                <div class="card-footer" v-if="linkMultiple">
-                    <LinkInput :link="linkMultiple"/>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <p>
-                Lub dopasuj bardziej szczegółowo:
-            </p>
-
-            <a v-if="!customise" href="#" @click.prevent="customise = true" class="btn btn-outline-primary btn-block">
-                <Icon v="sliders-h-square"/>
-                Kreator
-            </a>
-            <div v-else class="card mb-5">
-                <div class="card-header">
-                    <Icon v="sliders-h-square"/>
-                    Kreator
-                </div>
-                <div class="card-body">
-                    <div class="card-title border-bottom pb-3">
-                        <ul class="list-inline d-inline mb-0">
-                            <li class="list-inline-item pt-1 h5">
-                                Sugestie:
-                            </li>
-                            <li class="list-inline-item" v-for="(template, pronoun) in templates">
-                                <button :class="['btn', template.name() === selectedTemplate.name() ? 'btn-primary' : 'btn-outline-primary', 'btn-sm', 'my-1']"
-                                        @click="selectedTemplate = templates[pronoun].clone()"
-                                >
-                                    {{template.name()}}
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="alert alert-primary">
-                        <p class="h3 mb-0 text-center">
-                            {{ selectedTemplate.name() }}
-                            <br/>
-                            <input v-model="selectedTemplate.description"
-                                   class="form-control form-input p-0 form-control-sm"
-                                   :size="selectedTemplate.description.length + 3"
-                                   maxlength="48"
-                            />
-                        </p>
-                    </div>
-
-                    <p>
-                        Przykłady użycia w zdaniu:
+                </li>
+                <li class="list-group-item">
+                    <p class="h5">
+                        Generator form
                     </p>
-                    <template v-for="isHonorific in [false, true]">
-                        <ul>
-                            <li v-for="example in examples" v-if="example.isHonorific === isHonorific">
+                    <p>
+                        Możesz także użyć poniższego narzędzia, w którym uzupełnisz luki w zdaniach
+                        zgodnie z formami, których używasz, by wygenerować gotowy do udostępniania innym link.
+                    </p>
+                    <a v-if="!customise" href="#" @click.prevent="customise = true" class="btn btn-outline-primary btn-block">
+                        <Icon v="sliders-h-square"/>
+                        Pokaż generator
+                    </a>
+                    <div v-else class="card mb-5">
+                        <div class="card-header">
+                            <Icon v="sliders-h-square"/>
+                            Wygeneruj link
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title border-bottom pb-3">
+                                <ul class="list-inline d-inline mb-0">
+                                    <li class="list-inline-item pt-1 h5">
+                                        Na podstawie:
+                                    </li>
+                                    <li class="list-inline-item" v-for="(template, pronoun) in templates">
+                                        <button :class="['btn', template.name() === selectedTemplate.name() ? 'btn-primary' : 'btn-outline-primary', 'btn-sm', 'my-1']"
+                                                @click="selectedTemplate = templates[pronoun].clone()"
+                                        >
+                                            {{template.name()}}
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="alert alert-primary">
+                                <p class="h3 mb-0 text-center">
+                                    {{ selectedTemplate.name() }}
+                                    <br/>
+                                    <input v-model="selectedTemplate.description"
+                                           class="form-control form-input p-0 form-control-sm"
+                                           :size="selectedTemplate.description.length + 3"
+                                           maxlength="48"
+                                    />
+                                </p>
+                            </div>
+
+                            <p>
+                                Przykłady użycia w zdaniu:
+                            </p>
+                            <template v-for="isHonorific in [false, true]">
+                                <ul>
+                                    <li v-for="example in examples" v-if="example.isHonorific === isHonorific">
                                 <span v-for="part in example[(isHonorific ? selectedTemplate.pluralHonorific : selectedTemplate.plural) ? 'pluralParts' : 'singularParts']">
                                     <input v-if="part.variable" v-model="selectedTemplate.morphemes[part.str]"
                                            :class="['form-control form-input p-0', {'active': selectedMorpheme === part.str}]"
@@ -145,42 +136,44 @@
                                     />
                                     <span v-else>{{part.str}}</span>
                                 </span>
-                            </li>
-                        </ul>
-                        <div class="my-3">
-                            <div class="custom-control custom-switch" v-if="isHonorific">
-                                <input type="checkbox" class="custom-control-input" id="pluralHonorific" v-model="selectedTemplate.pluralHonorific">
-                                <label class="custom-control-label" for="pluralHonorific">Liczba mnoga <Icon v="level-up"/></label>
-                            </div>
-                            <div class="custom-control custom-switch" v-else>
-                                <input type="checkbox" class="custom-control-input" id="plural" v-model="selectedTemplate.plural">
-                                <label class="custom-control-label" for="plural">Liczba mnoga <Icon v="level-up"/></label>
+                                    </li>
+                                </ul>
+                                <div class="my-3">
+                                    <div class="custom-control custom-switch" v-if="isHonorific">
+                                        <input type="checkbox" class="custom-control-input" id="pluralHonorific" v-model="selectedTemplate.pluralHonorific">
+                                        <label class="custom-control-label" for="pluralHonorific">Liczba mnoga <Icon v="level-up"/></label>
+                                    </div>
+                                    <div class="custom-control custom-switch" v-else>
+                                        <input type="checkbox" class="custom-control-input" id="plural" v-model="selectedTemplate.plural">
+                                        <label class="custom-control-label" for="plural">Liczba mnoga <Icon v="level-up"/></label>
+                                    </div>
+                                </div>
+                            </template>
+                            <p class="small">
+                                <Icon v="info-circle"/>
+                                Możesz tu również wpisać formy wymienne w każdym polu z osobna, np. <code>jego&jej</code> = „jego” lub „jej”.
+                            </p>
+                            <div class="alert alert-warning">
+                                <p class="mb-0 small">
+                                    <Icon v="exclamation-triangle"/>
+                                    Strona jest w wersji βeta!
+                                    Przykłady, formy gramatyczne i linki mogą się jeszcze mocno zmienić!
+                                    Jeśli masz jakieś sugestie lub uwagi, daj mi proszę znać
+                                    <a href="https://twitter.com/AvrisIT" target="_blank" rel="noopener">na Twitterze</a>
+                                    lub
+                                    <a href="mailto:andrea@avris.it" target="_blank" rel="noopener">przez maila</a>.
+                                </p>
                             </div>
                         </div>
-                    </template>
-                    <p class="small">
-                        <Icon v="info-circle"/>
-                        Możesz tu również wpisać formy wymienne w każdym polu z osobna, np. <code>jego&jej</code> = „jego” lub „jej”.
-                    </p>
-                    <div class="alert alert-warning">
-                        <p class="mb-0 small">
-                            <Icon v="exclamation-triangle"/>
-                            Strona jest w wersji βeta!
-                            Przykłady, formy gramatyczne i linki mogą się jeszcze mocno zmienić!
-                            Jeśli masz jakieś sugestie lub uwagi, daj mi proszę znać
-                            <a href="https://twitter.com/AvrisIT" target="_blank" rel="noopener">na Twitterze</a>
-                            lub
-                            <a href="mailto:andrea@avris.it" target="_blank" rel="noopener">przez maila</a>.
-                        </p>
+                        <div class="card-footer" v-if="link">
+                            <LinkInput :link="link"/>
+                        </div>
+                        <div class="card-body border-top" v-if="Object.keys(sources).length">
+                            <Literature :sources="sources"/>
+                        </div>
                     </div>
-                </div>
-                <div class="card-footer" v-if="link">
-                    <LinkInput :link="link"/>
-                </div>
-                <div class="card-body border-top" v-if="Object.keys(sources).length">
-                    <Literature :sources="sources"/>
-                </div>
-            </div>
+                </li>
+            </ul>
         </section>
 
         <Separator icon="bookmark"/>
