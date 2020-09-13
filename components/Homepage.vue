a76578f9e6c9700f2fd68652142d6269ae044df5
Avris <andre@avris.it>
2020-09-13 21:08:19 +0200

    restore Homepage component

---
+++ components/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+    <p :class="['text-' + align, 'mb-0']">
+        <nuxt-link to="/" class="btn btn-outline-primary btn-sm">
+            <Icon v="home"/>
+            Strona główna
+        </nuxt-link>
+    </p>
+</template>
+
+<script>
+    export default {
+        props: {
+            align: { default: 'right' }
+        }
+    }
+</script>
