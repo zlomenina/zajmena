46ed6f0debf01075a22cd2a51982c7ad92ccf688
Avris <andre@avris.it>
2020-09-16 23:40:29 +0200

    #53 badge dla form normatywnych - hide under hoverable icon

---
+++ components/NormativeBadge.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="badge badge-secondary toggle-hover">
+        <Icon v="book-open"/>
+        <span class="togglable">Normatywne</span>
+    </span>
+</template>
+
+<style lang="scss" scoped>
+    .toggle-hover {
+        .togglable {
+            display: none;
+        }
+        &:hover {
+            .togglable {
+                display: inline;
+            }
+        }
+    }
+</style>
