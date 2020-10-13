89a9ed4bc29bb661fc1b4836dfd738c56a7ce540
Avris <andre@avris.it>
2020-10-13 18:29:42 +0200

    #54 user accounts - db

---
+++ migrations/002-users.sql
@@ -0,0 +1,23 @@
+-- Up
+
+CREATE TABLE users (
+    id TEXT NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    roles TEXT NOT NULL,
+    avatarSource TEXT
+);
+
+CREATE TABLE authenticators (
+    id TEXT NOT NULL PRIMARY KEY,
+    userId TEXT NOT NULL,
+    type TEXT NOT NULL,
+    payload TEXT NOT NULL,
+    validUntil INTEGER,
+    FOREIGN KEY(userId) REFERENCES users(id)
+);
+
+-- Down
+
+DROP TABLE authenticators;
+DROP TABLE users;
