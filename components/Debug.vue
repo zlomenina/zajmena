d49156537a73de4fb684343dd815711d5bf20db2
Avris <andre@avris.it>
2020-10-16 15:24:12 +0200

    debug component

---
+++ components/Debug.vue
@@ -0,0 +1,11 @@
+<template>
+    <pre>{{JSON.stringify(v, null, 4)}}</pre>
+</template>
+
+<script>
+    export default {
+        props: {
+            v: { required: true },
+        },
+    };
+</script>
