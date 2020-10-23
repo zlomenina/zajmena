ebf7064b4d59c925fb4f1b3f51071b1944e892db
Avris <andre@avris.it>
2020-10-23 19:06:39 +0200

    [pl][links] anglica, mistycyzm, rjp

--- components/Link.vue
+++ components/Link.vue
@@ -1,6 +1,7 @@
 <template>
     <li class="my-2">
         <Icon :v="link.icon" :set="link.iconSet || 'l'"/>
+        <span v-for="lang in link.lang || []" class="badge badge-light border">{{lang}}</span>
         <a :href="link.url" target="_blank" rel="noopener">
             <LinkedText :text="link.headline"/>
         </a>
