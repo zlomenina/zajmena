fab9c764c0db726196b509032d992ec6fc79529a
Avris <andre@avris.it>
2020-11-01 12:48:12 +0100

    [banner] fix routes with slashes

--- server/routes/banner.js
+++ server/routes/banner.js
@@ -24,7 +24,7 @@ const drawCircle = (context, image, x, y, size) => {
 
 const router = Router();
 
-router.get('/banner/:templateName.png', async (req, res) => {
+router.get('/banner/:templateName*.png', async (req, res) => {
     const width = 1200
     const height = 600
     const mime = 'image/png';
