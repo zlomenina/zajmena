7bf34ed9046192d4575b45dc5bfd9d2441ff8777
Avris <andre@avris.it>
2020-12-03 16:39:40 +0100

    #61 [en][sources]

--- components/Source.vue
+++ components/Source.vue
@@ -4,7 +4,7 @@
         <strong><template v-if="source.author">{{source.author.replace('^', '')}}</template><span v-if="source.author"> – </span><em><a v-if="source.link" :href="source.link" target="_blank" rel="noopener">{{source.title}}</a><span v-else>{{source.title}}</span></em></strong><template v-if="source.extra"> ({{source.extra}})</template>, {{source.year}}<template v-if="source.comment">; {{source.comment}}</template>
         <ul v-if="source.fragments.length">
             <li v-for="fragment in source.fragments">
-                „<span v-html="fragment.replace(/\n/g, '<br/>')"></span>”
+                <T>quotation.start</T><span v-html="fragment.replace(/\n/g, '<br/>')"></span><T>quotation.end</T>
             </li>
         </ul>
     </div>
