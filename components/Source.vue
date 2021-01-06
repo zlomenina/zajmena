ad4de1ed927fc77c647e462bd73619d1f8f62cb2
Avris <andre@avris.it>
2021-01-06 19:31:47 +0100

    [sources] nicer interface for images

--- components/Source.vue
+++ components/Source.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="my-2" v-if="!deleted">
-        <div v-if="source.images.length" class="float-right">
-            <ImageThumb v-for="image in source.images" :key="image" :id="image" class="m-2"/>
-        </div>
         <h3 class="h6">
             <Icon :v="source.icon()"/>
             <strong><template v-if="source.author">{{source.author.replace('^', '')}}</template><span v-if="source.author"> – </span><em><a v-if="source.link" :href="source.link" target="_blank" rel="noopener">{{source.title}}</a><span v-else>{{source.title}}</span></em></strong><template v-if="source.extra"> ({{source.extra}})</template>, {{source.year}}<template v-if="source.comment">; {{source.comment}}</template>
@@ -62,6 +59,9 @@
                 </span>
             </li>
         </ul>
+        <div v-if="source.images.length" class="float-right">
+            <ImageThumb v-for="image in source.images" :key="image" :id="image" class="m-2"/>
+        </div>
         <ul v-if="source.fragments.length">
             <li v-for="fragment in source.fragments">
                 <T>quotation.start</T><span v-html="fragment.replace(/\n/g, '<br/>')"></span><T>quotation.end</T>
