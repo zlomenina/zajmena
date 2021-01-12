61cf356f4416709f797a452d6beb4d5662d5d733
Avris <andre@avris.it>
2021-01-12 15:18:22 +0100

    [nl][pt] prepare base

---
+++ locale/nl/pronouns/morphemes.js
@@ -0,0 +1,13 @@
+export default [
+    'pronoun_n',
+    'pronoun_d',
+    'pronoun_a',
+    'pronoun_g',
+    'article_n',
+    'article_g',
+    'article_d',
+    'article_a',
+    'demonstrative_n',
+    'possesive_object',
+    'noun',
+];
