933dafbbe5d7f591f778f268fef7782c5cf704f6
Avris <andre@avris.it>
2021-01-12 15:34:46 +0100

    [nl] wip

--- locale/nl/pronouns/morphemes.js
+++ locale/nl/pronouns/morphemes.js
@@ -1,13 +1,7 @@
 export default [
-    'pronoun_n',
-    'pronoun_d',
-    'pronoun_a',
-    'pronoun_g',
-    'article_n',
-    'article_g',
-    'article_d',
-    'article_a',
-    'demonstrative_n',
-    'possesive_object',
-    'noun',
+    'nominative',
+    'accusative',
+    'pronominal_poss',
+    'predicative_poss',
+    'reflexive',
 ];
