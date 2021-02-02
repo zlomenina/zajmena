73fab096b9834b83bee0115fcf119ab47be65fa0
Avris <andre@avris.it>
2021-02-02 21:56:17 +0100

    spelling - fix handling empty tags

--- components/Spelling.vue
+++ components/Spelling.vue
@@ -7,7 +7,18 @@
             text: {},
         },
         render(h) {
-            return h('span', {domProps: { innerHTML: this.handleSpelling(this.text !== undefined ? this.text : this.$slots.default[0].text) }});
+            return h('span', {domProps: { innerHTML: this.handleSpelling(this.val) }});
         },
+        computed: {
+            val() {
+                if (this.text !== undefined) {
+                    return this.text;
+                }
+                if (this.$slots.default && this.$slots.default.length) {
+                    return this.$slots.default[0].text;
+                }
+                return ''
+            }
+        }
     }
 </script>
