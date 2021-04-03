7a324fe25d5e6c232a210fd87bba4819d252c37b
Avris <andre@avris.it>
2021-04-03 21:28:08 +0200

    [pt] release

--- src/locales.js
+++ src/locales.js
@@ -4,7 +4,7 @@ export default [
     ['en', 'English',    'https://en.pronouns.page', true],
     ['nl', 'Nederlands', 'https://nl.pronouns.page', true],
     ['pl', 'Polski',     'https://zaimki.pl',        true],
-    ['pt', 'Português',  'https://pt.pronouns.page', false],
+    ['pt', 'Português',  'https://pt.pronouns.page', true],
     ['ru', 'Русский',    'https://ru.pronouns.page', false],
     ['yi', 'ייִדיש',      'https://yi.pronouns.page', false],
     ['zh', '中文',        'https://zh.pronouns.page', true],
