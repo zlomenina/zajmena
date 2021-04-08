ec9e38c60ab70e5ac5fe630945ffe9364bcbe91d
Avris <andre@avris.it>
2021-04-08 17:55:48 +0200

    [fr] wip

--- src/locales.js
+++ src/locales.js
@@ -2,6 +2,7 @@ export default [
     ['de', 'Deutsch',    'https://de.pronouns.page', false],
     ['es', 'Español',    'https://es.pronouns.page', true],
     ['en', 'English',    'https://en.pronouns.page', true],
+    ['fr', 'Français',   'https://fr.pronouns.page', false],
     ['nl', 'Nederlands', 'https://nl.pronouns.page', true],
     ['pl', 'Polski',     'https://zaimki.pl',        true],
     ['pt', 'Português',  'https://pt.pronouns.page', true],
