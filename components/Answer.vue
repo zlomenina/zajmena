014c1781345fee52ecdfc836d125a9e96c19d9c0
Avris <andre@avris.it>
2021-04-08 18:15:54 +0200

    [faq] fix hash

--- components/Answer.vue
+++ components/Answer.vue
@@ -1,5 +1,5 @@
 <template>
-    <details v-if="$t('faq.questions.' + question, [], false)" :class="['border mb-3', small ? 'small' : '']">
+    <details v-if="$t('faq.questions.' + question, [], false)" :class="['border mb-3', small ? 'small' : '']" @click="$emit('click')">
         <summary :class="['bg-light', small ? 'p-2' : 'p-3']"><T>faq.questions.{{question}}.question</T></summary>
         <div :class="['border-top', small ? 'p-2' : 'p-3']">
             <T>faq.questions.{{question}}.answer</T>
