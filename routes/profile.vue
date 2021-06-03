ade526864a6ff0ab3ec0ea2912d760947321cf52
Avris <andre@avris.it>
2021-06-03 22:53:57 +0200

    #213 flags don't show when only custom flags added

--- routes/profile.vue
+++ routes/profile.vue
@@ -41,7 +41,7 @@
             </p>
         </section>
 
-        <section v-if="profile.flags.length || profile.customFlags.length">
+        <section v-if="profile.flags.length || Object.keys(profile.customFlags).length">
             <ul class="list-inline">
                 <li v-for="flag in profile.flags" v-if="allFlags[flag]" class="list-inline-item pr-2">
                     <Flag :name="flag.startsWith('-') ? allFlags[flag] : $translateForPronoun(allFlags[flag], mainPronoun)"
